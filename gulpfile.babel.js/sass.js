f04384fae3f1dd1f2318dc846b2ad6514c639536
Samuel Gomez <contact@samuelgomez.fr>
2019-04-29 23:32:57 +0200

    update packages

--- gulpfile.babel.js/sass.js
+++ gulpfile.babel.js/sass.js
@@ -5,7 +5,7 @@ import autoprefixer from 'gulp-autoprefixer';
 import sass from 'gulp-sass';
 import nodesass from 'node-sass';
 import plumber from 'gulp-plumber';
-import minifyCSS from 'gulp-csso';
+import cleanCSS from 'gulp-clean-css';
 import { reload, notify } from './serve';
 import config from './config';
 
@@ -14,44 +14,46 @@ sass.compiler = nodesass;
 const { pathSrc, pathDest, sassIndex } = config;
 
 /* Production ***************************** */
-export const sassBuild = () => src(`${pathSrc}${sassIndex}`)
-  .pipe(
-    sass({
-      includePaths: ['node_modules'],
-    }).on('error', sass.logError),
-  )
-  .pipe(
-    autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false,
-    }),
-  )
-  .pipe(minifyCSS())
-  .pipe(concat('bundle.css'))
-  .pipe(dest(pathDest));
+export const sassBuild = () =>
+  src(`${pathSrc}${sassIndex}`)
+    .pipe(
+      sass({
+        includePaths: ['node_modules'],
+      }).on('error', sass.logError),
+    )
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false,
+      }),
+    )
+    .pipe(cleanCSS())
+    .pipe(concat('bundle.css'))
+    .pipe(dest(pathDest));
 
 /* Development ***************************** */
-const sassDev = () => src(`${pathSrc}${sassIndex}`)
-  .pipe(plumber())
-  .pipe(sourcemaps.init())
-  .pipe(
-    sass({
-      includePaths: ['node_modules'],
-    }).on('error', ({ message }) => {
-      console.error(message);
-      notify(message, 3000);
-      this.emit('end');
-    }),
-  )
-  .pipe(
-    autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false,
-    }),
-  )
-  .pipe(concat('bundle.css'))
-  .pipe(sourcemaps.write())
-  .pipe(dest(pathDest))
-  .pipe(reload({ stream: true }));
+const sassDev = () =>
+  src(`${pathSrc}${sassIndex}`)
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({
+        includePaths: ['node_modules'],
+      }).on('error', ({ message }) => {
+        console.error(message);
+        notify(message, 3000);
+        this.emit('end');
+      }),
+    )
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false,
+      }),
+    )
+    .pipe(concat('bundle.css'))
+    .pipe(sourcemaps.write())
+    .pipe(dest(pathDest))
+    .pipe(reload({ stream: true }));
 
 export default sassDev;
